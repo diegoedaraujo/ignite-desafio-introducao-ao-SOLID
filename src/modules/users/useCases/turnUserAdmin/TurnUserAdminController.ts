9aabebb29517b508d6cafd43283df4b391215e36
Diego Araújo <diegoedaraujo@gmail.com>
2022-04-12 15:29:44 -0300

    Implementando

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,12 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
